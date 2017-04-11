57ec7bce95bafae1c3641e41efaba3880089f914
Christopher <rivas1@cpp.edu>
2017-04-11 16:17:40 -0700

    Added basic stream readers
    Compiles properly.

--- ChatClient.java
+++ ChatClient.java
@@ -1,8 +1,22 @@
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.IOException;
 
 public final class ChatClient
 {
-	public static void main (string[] args)
+	public static void main (String[] args) throws IOException
 	{
-		
+		try 
+		{
+			Socket socket = new Socket ("codebank.xyz", 38001);
+			InputStream is = socket.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+			BufferedReader br = new BufferedReader(isr);
+		}
+		catch (IOException e)
+		{ e.printStackTrace(); }
 	}
 }
