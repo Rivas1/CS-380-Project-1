7a1c286c281595bb0ef57a8cbab490670a225bc5
Christopher <rivas1@cpp.edu>
2017-04-12 19:22:40 -0700

    Program is complete
    Removed debugging statements.
    When ran, the program will connect to the server at "codebank.xyz" with
    port 38001.
    Next, it will prompt user to enter a username; if username is taken,
    server will respond with "Name in use." and program will quit.
    Otherwise, program will infinitely allow user to enter messages to send
    to all clients connected to the server.

--- ChatClient.java
+++ ChatClient.java
@@ -11,9 +11,7 @@ public final class ChatClient
 		Scanner keyboard = new Scanner (System.in);
 		try 
 		{
-			System.out.println("Waiting to connect to server...");
 			Socket socket = new Socket ("codebank.xyz", 38001);
-			System.out.println("Successfully connected!");
 			PrintStream PS = new PrintStream(socket.getOutputStream()); // out from client to server
 			System.out.print("Please enter a username: ");
 			PS.println(keyboard.nextLine()); // prompt user for username
@@ -29,22 +27,7 @@ public final class ChatClient
 					System.exit(1);
 				PS.println(keyboard.nextLine()); // sends user typed message to server
 			}
-			
-			
-			/*
-			while (true)
-			{
-				InputStreamReader IR = new InputStreamReader(socket.getInputStream()); // listen to server
-				BufferedReader BR = new BufferedReader(IR);
 
-				serverOutput = BR.readLine();
-				System.out.println("Server: " + serverOutput);
-				PS.println("hello");
-				break;
-//				PS.println(keyboard.nextLine());
-			}
-			*/
-			
 		}
 		catch (IOException e)
 		{ e.printStackTrace(); }
