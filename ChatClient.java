0035e6ce6bff85680066cc3dccb272c03fb03d7a
Christopher <rivas1@cpp.edu>
2017-04-12 18:42:02 -0700

    Connects to Server + Sends username with server response
    Successfully connects to server.
    Sends a pre-typed message as username and server responds with "name in
    use".
    Loops infinitely as it takes client input via keyboard + server responds
    with "null".

--- ChatClient.java
+++ ChatClient.java
@@ -1,20 +1,32 @@
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.net.Socket;
-import java.net.ServerSocket;
-import java.io.IOException;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
 public final class ChatClient
 {
 	public static void main (String[] args) throws IOException
 	{
+		String serverOutput = "";
+		String clientInput = "";
+		Scanner keyboard = new Scanner (System.in);
 		try 
 		{
+			System.out.println("Waiting to connect to server...");
 			Socket socket = new Socket ("codebank.xyz", 38001);
-			InputStream is = socket.getInputStream();
-			InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-			BufferedReader br = new BufferedReader(isr);
+			System.out.println("Successfully connected!");
+			PrintStream PS = new PrintStream(socket.getOutputStream()); // out from client to server
+			PS.println("rivas1");
+//			System.out.println("Test!");
+			while (true)
+			{
+				InputStreamReader IR = new InputStreamReader(socket.getInputStream()); // listen to server
+				BufferedReader BR = new BufferedReader(IR);
+
+				serverOutput = BR.readLine();
+				System.out.println("Server: " + serverOutput);
+				PS.println(keyboard.nextLine());
+			}
+			
 		}
 		catch (IOException e)
 		{ e.printStackTrace(); }
